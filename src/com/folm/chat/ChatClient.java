4d36153cb5dd56612f31b439336d5ebe36769b0d
freedomFu <174780597@qq.com>
2019-05-21 20:40:13 +0800

    could not found

--- src/com/folm/chat/ChatClient.java
+++ src/com/folm/chat/ChatClient.java
@@ -2,8 +2,18 @@ package com.folm.chat;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class ChatClient extends JFrame {
+
+    Socket s = null;
+    DataOutputStream dos = null;
 
     TextField tfTxt = new TextField();
     TextArea taContent = new TextArea();
@@ -20,8 +30,53 @@ public class ChatClient extends JFrame {
         add(taContent, BorderLayout.NORTH);
         pack();
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                disconnect();
+                System.exit(0);
+            }
+        });
+
+        tfTxt.addActionListener(new TFListener());
         setVisible(true);
+        connect();
+    }
+
+    public void connect(){
+        try {
+            s = new Socket("127.0.0.1", 8888);
+            dos = new DataOutputStream(s.getOutputStream());
+            System.out.println("connected!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect(){
+        try {
+            dos.close();
+            s.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class TFListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // 拿到字符串 如何写到服务器端呢
+            String str = tfTxt.getText().trim();
+            taContent.append(str+"\n");
+            tfTxt.setText("");
+            try {
+                dos.writeUTF(str);
+                dos.flush();
+//                dos.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
     }
 
 }
